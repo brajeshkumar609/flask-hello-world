b98417d401a14d78886051f93e22bfca4e28e31d
Ubuntu <azureuser@azure.i1ccrovwkp2ube3uomrrikfwmc.ax.internal.cloudapp.net>
2024-07-16 20:58:09 +0000

    ji ki bahi log

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Jai shiya ram Ji kIi log'
+   return 'Jai shiya ram Ji akchansh log'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
